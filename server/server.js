fbd8d9ee3d232971023f79593dbef34da91a9a3e
lemesa kasim <alikasim04451530@gmail.com>
2024-07-02 16:37:49 +0300

    other cors commit

--- server/server.js
+++ server/server.js
@@ -13,27 +13,6 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://feedback-system-1.onrender.com",
-    origin: "http://localhost:3000", // Allow only the frontend server's origin
-  })
-);
-require("dotenv").config();
-
-const express = require("express");
-const mongoose = require("mongoose");
-const workoutRoutes = require("./routes/workouts");
-const userRoutes = require("./routes/user");
-const complaintRouter = require("./routes/complaint");
-const cors = require("cors");
-const path = require("path");
-
-// express app
-const app = express();
-
-// middleware
-app.use(express.json());
 
 // Corrected CORS configuration
 app.use(
@@ -57,34 +36,6 @@ app.use("/api/complaint", complaintRouter);
 app.use(express.static(path.join(__dirname, "/client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/build/index.html"));
-});
-
-// connect to db
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    // listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log("connected to db & listening on port", process.env.PORT);
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
-
-// routes
-app.use("/api/workouts", workoutRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/complaint", complaintRouter);
-
-app.use(express.static(path.join(__dirname, "/client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/build"));
 });
 
 // connect to db
